7c12a3cd0218f15e536c385c0d7f2c64b8edcec9
Larry Naman <lawrence.naman.1@city.ac.uk>
2013-08-14 10:01:06 +0100

    more clean up

--- css/config.rb
+++ css/config.rb
@@ -4,20 +4,13 @@ http_path = ""
 css_dir = ""
 sass_dir = "src"
 images_dir = "../i"
-http_images_path = "/i"
+http_images_path = "../i"
 javascripts_dir = "../js"
-http_javascripts_path = "js"
+http_javascripts_path = "../js"
+
 
 #lets enable the sass debug info
-sass_options = {:debug_info => false, :quiet => true}
+sass_options = {:debug_info => true, :quiet => true}
 
 #options for output_style: ":nested", ":expanded", ":compact", ":compressed"
 output_style = :compressed
-
-#kill the built in cache buster
-asset_cache_buster :none
-
-#this will define the absolute path to images referenced in the stylesheets
-asset_host do |asset|
-  "http://webapps.dev.city.ac.uk/service-desk-student/"
-end
